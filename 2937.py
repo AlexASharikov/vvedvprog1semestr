5fc31fbcf8984f1371e1915d02c4852cc2367c1f
AlexASharikov <113688264+AlexASharikov@users.noreply.github.com>
2022-09-16 14:47:50 +0300

    Add files via upload

---
+++ 2937.py
@@ -0,0 +1,5 @@
+a = int(input())
+a1 = a + 1
+a2 = a - 1
+print("The next number for the number ",a," is ",a1,".", sep="")
+print("The previous number for the number ",a," is ",a2,".", sep="")
---
+++ 3465.py
@@ -0,0 +1,3 @@
+a = int(input())
+a1 = (a // 2 + 1) * 2
+print(a1)
